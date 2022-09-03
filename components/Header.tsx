3e01af592c4a294c4c8c72656d287245d00a0306
Enhmunh-E <enhuush0704@gmail.com>
2022-09-03 19:27:10 +0800

    Feature - Entry page ongoing

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import TuuriLogo from "../public/assets/tuuriLogo.svg";
+export const Header = () => {
+  return (
+    <header style={styles.headerContainer}>
+      <div className="middleContainer">
+        <Image src={TuuriLogo} />
+      </div>
+    </header>
+  );
+};
+const styles: Record<string, React.CSSProperties> = {
+  headerContainer: {
+    height: "60px",
+    borderBottom: "1px solid #FFC909",
+    display: "flex",
+    alignItems: "center",
+  },
+};
