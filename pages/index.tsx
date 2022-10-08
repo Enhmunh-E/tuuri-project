c6753fbb6f2b1569ad46f14957a96ea576352016
Enhmunh-E <enhuush0704@gmail.com>
2022-10-08 19:11:59 +0800

    Feature - Contentful Connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,25 @@ import type { NextPage } from "next";
 import { Header } from "../components";
 import styles from "../styles/Home.module.css";
 import dynamic from "next/dynamic";
+import { createClient, Entry } from "contentful";
 
 const Pixi = dynamic(import("../components/Pixi"), { ssr: false });
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  });
+  const res = await client.getEntries({ content_type: "article" });
+  return {
+    props: {
+      articles: res.items,
+    },
+  };
+};
+
+const Home = ({ articles }: { articles: Entry<unknown>[] }) => {
+  console.log(articles);
   return (
     <div className={styles.container}>
       <Header />
