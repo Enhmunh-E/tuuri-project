439a83bd395f18609f3540f6cb38d8639a51d2b4
n.nekogasuki643 <n.nekogasuki643@gmail.com>
2022-10-25 09:37:42 +0800

    list takes real articles

--- components/List.tsx
+++ components/List.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useMainProvider, useScrollContext } from "../providers";
 
-const List = ({ data }: any) => {
-  const { scroll, setScroll } = useScrollContext();
-  const { currentDataIndex, setCurrentDataIndex } = useMainProvider();
+const List = () => {
+  const { scroll } = useScrollContext();
+  const { setCurrentDataIndex, allArticles } = useMainProvider();
   const scrollRef = useRef<HTMLDivElement>(null);
   const [scrollTop, setScrollTop] = useState(0);
+
+  console.log(allArticles);
 
   useEffect(() => {
     const nextTop = Math.min(Math.max(scrollTop + scroll, 0), 440);
@@ -21,11 +23,11 @@ const List = ({ data }: any) => {
 
   return (
     <div className="list" ref={scrollRef}>
-      {data.map((d: any, index: number) => {
-        const { title, year } = d;
+      {allArticles?.map((d: any, index: number) => {
+        const { title, eventDate } = d;
         return (
           <div className="list-element" key={title}>
-            <div className="year">{year}</div>
+            <div className="year">{eventDate}</div>
             <div className="line"></div>
             <div className="title">{title}</div>
           </div>
