1079c02a82faa4052fad58c95854f88bdc0128ff
jangar <jangar551@gmail.com>
2022-11-16 20:56:34 +0800

    Enchancement - Google meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="google-site-verification"
+            content="qTBtbv1zg90s2WmPQyZagDkToZ2J58a8wVbWcp13vQw"
+          />
         </Head>
         <body>
           <Main />
