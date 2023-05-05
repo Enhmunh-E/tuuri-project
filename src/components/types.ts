5fa9ef0bfdc23c569bae676b2b36192cbc10b933
Akiko <amarjargal.a@steppelink.mn>
2023-05-06 03:02:59 +0800

    filter works

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,53 +1,58 @@
 export type ArticleType = {
-  fields: {
-    title: string;
-    coverPic: {
-      fields: {
-        file: {
-          url: string;
-        };
-      };
-    };
-    eventDate: number;
-    readTime: number;
-    topic: string[];
-    backgroundImage: {
-      fields: {
-        description: string;
-        file: {
-          url: string;
-        };
+    fields: {
         title: string;
-      };
+        coverPic: {
+            fields: {
+                file: {
+                    url: string;
+                };
+            };
+        };
+        eventDate: number;
+        readTime: number;
+        topic: string[];
+        backgroundImage: {
+            fields: {
+                description: string;
+                file: {
+                    url: string;
+                };
+                title: string;
+            };
+        };
+        blocks: BlockType[];
     };
-    blocks: BlockType[];
-  };
-  sys: {
-    contentType: {
-      sys: {
-        id: string;
-      };
+    sys: {
+        contentType: {
+            sys: {
+                id: string;
+            };
+        };
     };
-  };
 };
 
 export type BlockType = {
-  image?: {
-    fields: {
-      description: string;
-      file: {
-        url: string;
-      };
-      title: string;
+    image?: {
+        fields: {
+            description: string;
+            file: {
+                url: string;
+            };
+            title: string;
+        };
     };
-  };
-  // texts?: {
-  //   fields: {
-  //     text: {
-  //       fields: {
-  //         text: string;
-  //       };
-  //     };
-  //   };
-  // }[];
+    fields?: {
+        texts?: any;
+        title?: any;
+        image?: any;
+    };
+    // texts?: {
+    //   fields: {
+    //     text: {
+    //       fields: {
+    //         text: string;
+    //       };
+    //     };
+    //   };
+    // }[];
 };
