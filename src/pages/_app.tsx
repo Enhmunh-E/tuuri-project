2e22a55ff7b54fc5a08380dbfe0d3e98ec4e7677
Enhmunh-E <enhuush0704@gmail.com>
2023-05-06 00:43:12 +0800

    Feat - Go to desktop web

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,14 @@ import type { AppProps } from "next/app";
 import { MainProvider } from "../providers";
 import { Header } from "@components";
 import { Analytics } from "@vercel/analytics/react";
+import { useLayoutEffect } from "react";
+import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }: AppProps) {
+  useLayoutEffect(() => {
+    if (window?.innerWidth >= 1024) {
+      window.location.href = "https://tuuri.mn";
+    }
+  }, []);
   return (
     <>
       <MainProvider>
