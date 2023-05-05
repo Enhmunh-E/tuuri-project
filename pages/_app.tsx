78f2a05b9f1ce0dda176d57a1135a5fc9cad7bf3
Enhmunh-E <enhuush0704@gmail.com>
2023-05-06 00:46:06 +0800

    Feat - Go To Mobile when screen is small

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import type { AppProps } from "next/app";
 import { MainProvider } from "../providers";
 import { Header } from "../components";
 import { Analytics } from "@vercel/analytics/react";
+import { useLayoutEffect } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
+  useLayoutEffect(() => {
+    if (window?.innerWidth < 1024) {
+      window.location.href = "https://m.tuuri.mn";
+    }
+  }, []);
   return (
     <>
       <MainProvider>
