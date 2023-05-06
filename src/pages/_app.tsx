dc71a5dc6922fa3da296070f0863cf16bd3873a5
Enhmunh-E <enhuush0704@gmail.com>
2023-05-06 16:12:28 +0800

    Feat - Mobile loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,30 @@ import type { AppProps } from "next/app";
 import { MainProvider } from "../providers";
 import { Header } from "@components";
 import { Analytics } from "@vercel/analytics/react";
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { useRouter } from "next/router";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import Head from "next/head";
+
+NProgress.configure({ showSpinner: false });
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
   useLayoutEffect(() => {
     if (window?.innerWidth >= 1024) {
       window.location.href = "https://tuuri.mn";
