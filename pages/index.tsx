897885603a5d24144d1613fa3d3db535c5f33713
Akiko <amarjargal.a@steppelink.mn>
2023-05-06 16:27:57 +0800

    Feat - Sort articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,60 +14,66 @@ import Search from "../components/Search";
 const Pixi = dynamic(import("../components/Pixi"), { ssr: false });
 
 const Home = ({ articles }: { articles: ArticleType[] }) => {
-  const { setAllArticles, currentDataIndex } = useMainProvider();
-  const [transition, setTransition] = useState(false);
-  const dummy: ArticleType = {} as ArticleType;
-  useEffect(() => {
-    if (articles.length == 0) return;
-    setAllArticles(articles);
-  }, [articles, setAllArticles]);
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      setTransition(true);
-    }, 400);
-    return () => clearTimeout(timeOut);
-  }, []);
-  // if (loading) return <HomeLoading />;
-  return (
-    <div
-      className={styles.container}
-      style={{
-        transition: "all 400ms",
-        opacity: transition ? 1 : 0,
-      }}
-    >
-      <div className="rel">
-        <ScrollProvider>
-          <div
-            id="pixi-container"
+    const { setAllArticles, currentDataIndex } = useMainProvider();
+    const [transition, setTransition] = useState(false);
+    const dummy: ArticleType = {} as ArticleType;
+    useEffect(() => {
+        if (articles.length == 0) return;
+        setAllArticles(articles);
+    }, [articles, setAllArticles]);
+    useEffect(() => {
+        const timeOut = setTimeout(() => {
+            setTransition(true);
+        }, 400);
+        return () => clearTimeout(timeOut);
+    }, []);
+    // if (loading) return <HomeLoading />;
+    return (
+        <div
+            className={styles.container}
             style={{
-              display: "inline-flex",
-              height: "100vh",
-              width: "100vw",
+                transition: "all 400ms",
+                opacity: transition ? 1 : 0,
             }}
-          >
-            <Article setTransition={setTransition} />
-            <Pixi />
-          </div>
-          <List />
-        </ScrollProvider>
-        <div className="search-container">
-          <Search />
+        >
+            <div className="rel">
+                <ScrollProvider>
+                    <div
+                        id="pixi-container"
+                        style={{
+                            display: "inline-flex",
+                            height: "100vh",
+                            width: "100vw",
+                        }}
+                    >
+                        <Article setTransition={setTransition} />
+                        <Pixi />
+                    </div>
+                    <List />
+                </ScrollProvider>
+                <div className="search-container">
+                    <Search />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export async function getStaticProps() {
-  const res = await fetchEntries();
+    const res = await fetchEntries();
 
-  const articles = res?.filter((p) => p.sys.contentType.sys.id == "article");
-  return {
-    props: {
-      articles,
-    },
-  };
+    const articles = res
+        ?.filter((p) => p.sys.contentType.sys.id == "article")
+        .sort((a, b) => {
+            if (a.fields.eventDate < b.fields.eventDate) return -1;
+            if (b.fields.eventDate < a.fields.eventDate) return 1;
+            return 0;
+        });
+    return {
+        props: {
+            articles,
+        },
+    };
 }
 
 export default Home;
